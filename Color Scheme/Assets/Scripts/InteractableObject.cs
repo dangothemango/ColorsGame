ffa0a9d9717469fdb9408c356fdca51b11ceaa25
Dan Gorman <danthemangorman@gmail.com>
2018-02-14 14:37:57 -0500

    added c# scripts for a lot of stuff

---
+++ Color Scheme/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour {
+
+    private void Awake() {
+        DoAwake();
+    }
+
+    protected virtual void DoAwake() {}
+
+	// Use this for initialization
+	void Start () {
+        DoStart();
+	}
+
+    protected virtual void DoStart() {}
+	
+	// Update is called once per frame
+	void Update () {
+        DoUpdate();
+	}
+
+    protected virtual void DoUpdate() {}
+
+    public virtual void Interact() {
+        throw new System.NotImplementedException("Function must be overrided");
+    }
+}
