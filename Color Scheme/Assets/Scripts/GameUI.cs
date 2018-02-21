76ac2a69593269fafe3170520fc7cd0b24d8aebe
Elaine Chao <chaoe@rpi.edu>
2018-02-20 23:47:59 -0500

    Dungeon walls fixed, player can walk through all of it

---
+++ Color Scheme/Assets/Scripts/GameUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    public RawImage uiimage;
+    public Text txt;
+
+    void Start()
+    {
+        uiimage = GetComponent<RawImage>();
+        uiimage.enabled = false;
+        txt.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.M) && uiimage.name == "mapui")
+        {
+            uiimage.enabled = !uiimage.enabled;
+        }
+        else if (Input.GetKeyUp(KeyCode.I) && uiimage.name == "bucketinv")
+        {
+            uiimage.enabled = !uiimage.enabled;
+        }
+        else if (Input.GetKeyUp(KeyCode.C) && uiimage.name == "bucketcol")
+        {
+            uiimage.enabled = !uiimage.enabled;
+        }
+    }
+}
