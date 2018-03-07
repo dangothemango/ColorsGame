01dcb47c9399d93e65c9f821a8736f6ae2b16dc3
eti-yen <etiennem@sbcglobal.net>
2018-03-07 03:44:34 -0500

    Spoke 1 Room 2

---
+++ Color Scheme/Assets/Scripts/Second Dungeon Logic/OpenWhenRightColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenWhenRightColor : ButtonableObject 
+{
+	[SerializeField] ButtonActivatedDoor[] doors;
+	[SerializeField] Color correctColor;
+
+	public override void OnPressed(Color c)
+	{
+		if (c == correctColor)
+		{
+			foreach (ButtonActivatedDoor door in doors)
+				door.TriggerOpen();
+		}
+	}
+}
