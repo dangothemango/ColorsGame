bd640da3ba852990cca97661b54f5ba99a269d25
Dan Gorman <danthemangorman@gmail.com>
2018-03-07 02:17:57 -0500

    added the flashlight

---
+++ Color Scheme/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : PlayerItem {
+
+    static Color[] colorList = { Color.red, Color.green, Color.blue, Color.yellow, Color.magenta, Color.cyan, Color.white };
+
+    [Header("Object References")]
+    public Battery battery;
+    public Renderer colorableSurface;
+
+    int colorIndex = 0;
+    Color currentColor;
+    bool on = false;
+
+    bool IsOn {
+        get {
+            return on;
+        }
+        set {
+            on = value;
+            if (value) {
+                battery.Paint(currentColor);
+            } else {
+                battery.Paint(Color.black);
+            }
+        }
+    }
+
+
+    Color CurrentColor {
+        get {
+            return currentColor;
+        } set {
+            currentColor = value;
+            colorableSurface.material.color = value;
+            if (IsOn) {
+                battery.Paint(value);
+            }
+        }
+    }
+    public override bool CanUseOn(InteractableObject target) {
+        return true;
+    }
+
+    public override void Filter(Color c) {}
+
+    public override void SecondaryUsage() {
+        CurrentColor = colorList[(++colorIndex)%colorList.Length];
+    }
+
+    public override void UseOn(InteractableObject target) {
+        IsOn = !IsOn;
+    }
+
+    private void Awake() {
+        CurrentColor = colorList[colorIndex];
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Color Scheme/Assets/Scripts/Player.cs
+++ Color Scheme/Assets/Scripts/Player.cs
@@ -58,16 +58,15 @@ public class Player : MonoBehaviour {
 			setItem(items[0]);
 		}
 
-		if (Input.GetKeyDown(GameManager.INSTANCE.INTERACT))
-		{
-			if (gazedObject != null)
-			{
-				if (equippedItem != null && equippedItem.CanUseOn(gazedObject))
-					equippedItem.UseOn(gazedObject);
-				else
-					gazedObject.Interact();
+		if (Input.GetKeyDown(GameManager.INSTANCE.INTERACT)) { 
+		
+            if (equippedItem != null && equippedItem.CanUseOn(gazedObject)) {
+                equippedItem.UseOn(gazedObject);
+            } else if (gazedObject != null) {
+			    gazedObject.Interact();
 			}
 		}
+
 		else if (Input.GetKeyDown(GameManager.INSTANCE.ITEM_SECONDARY) && equippedItem != null)
 			equippedItem.SecondaryUsage();
 		
