7f7145f934575146c4ca197721e372f407435a30
Elaine Chao <chaoe@rpi.edu>
2018-03-07 05:34:13 -0500

    Shade AIs, Dungeon1 ledge fixes and railings

---
+++ Color Scheme/Assets/Scripts/RaceShade.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceShade : SimplePaintableObject
+{
+    Renderer r;
+    public Transform target;
+    public float maxSpeed;
+
+    public Color shadeColor;
+    public Color change;
+    public Color killColor;
+
+    void Start()
+    {
+        enabled = true;
+    }
+
+    void Update()
+    {
+        if (enabled)
+        {
+            float step = maxSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+    }
+
+    IEnumerator changeColor(GameObject obj)
+    {
+        yield return new WaitForSecondsRealtime(3);
+        obj.GetComponent<Renderer>().material.color = change;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        // Shade collided with paintable object
+        if (col.gameObject.name == "PointLight")
+        {
+            enabled = false;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            StartCoroutine(changeColor(col.gameObject));
+        }
+        else
+        {
+            Physics.IgnoreCollision(col.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+    }
+
+    public override void Paint(Color c)
+    {
+        if (c == killColor)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Color Scheme/Assets/Scripts/ShadeAI.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadeAI : SimplePaintableObject
+{
+    Renderer r;
+
+    public float maxSpeed;
+
+    public float highX;
+    public float highY;
+    public float highZ;
+    public float lowX;
+    public float lowY;
+    public float lowZ;
+
+    private float x;
+    private float y;
+    private float z;
+    private float time;
+    private float angle;
+
+    public Color shadeColor;
+    public Color[] changeArray;
+    public Color killColor;
+
+    void Start()
+    {
+        x = Random.Range(-maxSpeed, maxSpeed);
+        y = Random.Range(-maxSpeed, maxSpeed);
+        z = Random.Range(-maxSpeed, maxSpeed);
+        angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+        transform.localRotation = Quaternion.Euler(0, angle, 0);
+        enabled = true;
+    }
+
+    void Update()
+    {
+        if (enabled)
+        {
+            time += Time.deltaTime;
+
+            if (time > 1.0f)
+            {
+                x = Random.Range(-maxSpeed, maxSpeed);
+                z = Random.Range(-maxSpeed, maxSpeed);
+                angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 90;
+                transform.localRotation = Quaternion.Euler(0, angle, 0);
+                time = 0.0f;
+            }
+
+            transform.localPosition = new Vector3(transform.localPosition.x + x, transform.localPosition.y + y, transform.localPosition.z + z);
+        }
+    }
+
+    void deflect()
+    {
+        if (transform.localPosition.x > highX)
+        {
+            x = Random.Range(-maxSpeed, 0.0f);
+            angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 45;
+            transform.localRotation = Quaternion.Euler(0, angle, 0);
+            time = 0.0f;
+        }
+        if (transform.localPosition.x < lowX)
+        {
+            x = Random.Range(0.0f, maxSpeed);
+            angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 45;
+            transform.localRotation = Quaternion.Euler(0, angle, 0);
+            time = 0.0f;
+        }
+        if (transform.localPosition.y > highY)
+        {
+            y = Random.Range(-maxSpeed, 0.0f);
+            angle = Mathf.Atan2(y, x) * (180 / 3.141592f) + 45;
+            transform.localRotation = Quaternion.Euler(0, angle, 0);
+            time = 0.0f;
+        }
+        if (transform.localPosition.y < lowY)
+        {
+            y = Random.Range(0.0f, maxSpeed);
+            angle = Mathf.Atan2(y, x) * (180 / 3.141592f) + 45;
+            transform.localRotation = Quaternion.Euler(0, angle, 0);
+            time = 0.0f;
+        }
+        if (transform.localPosition.z > highZ)
+        {
+            z = Random.Range(-maxSpeed, 0.0f);
+            angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 45;
+            transform.localRotation = Quaternion.Euler(0, angle, 0);
+            time = 0.0f;
+        }
+        if (transform.localPosition.z < lowZ)
+        {
+            z = Random.Range(0.0f, maxSpeed);
+            angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 45;
+            transform.localRotation = Quaternion.Euler(0, angle, 0);
+            time = 0.0f;
+        }
+
+        transform.localPosition = new Vector3(transform.localPosition.x + x, transform.localPosition.y + y, transform.localPosition.z + z);
+    }
+
+    IEnumerator changeColor(GameObject obj)
+    {
+        yield return new WaitForSecondsRealtime(3);
+        int index = Random.Range(0, changeArray.Length);
+        obj.GetComponent<Renderer>().material.color = changeArray[index];
+        enabled = true;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        // Shade collided with paintable object
+        if (col.gameObject.name == "Cube")
+        {
+            int rng = Random.Range(1, 11);
+            // 70% chance that the shade stops to change object color
+            if (rng < 7)
+            {
+                enabled = false;
+                GetComponent<Rigidbody>().velocity = Vector3.zero;
+                GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                StartCoroutine(changeColor(col.gameObject));
+            }
+            deflect();
+        }
+        // Shade collided with wall
+        else if (col.gameObject.name == "Wall")
+        {
+            deflect();
+        }
+        else
+        {
+            // Shade passes through object
+            if (col.gameObject.name == "Cylinder")
+            {
+                Physics.IgnoreCollision(col.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
+            }
+        }
+    }
+
+    public override void Paint(Color c)
+    {
+        if (c == killColor)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
