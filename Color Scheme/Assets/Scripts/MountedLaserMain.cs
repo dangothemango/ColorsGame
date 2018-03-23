95de044bd901fe052dd1acfe77463bfa559cc3cf
khabane <seretsi@users.noreply.github.com>
2018-03-23 00:25:22 -0400

    documented laser gun script

--- Color Scheme/Assets/Scripts/MountedLaserMain.cs
+++ Color Scheme/Assets/Scripts/MountedLaserMain.cs
@@ -44,14 +44,16 @@ public class MountedLaserMain : InteractableObject {
 		laserRing = this.gameObject.transform.GetChild(1).gameObject;
 		laserRingSmall = this.gameObject.transform.GetChild(2).gameObject;
 
+		//restore variables for each child of the laser gun object
+		//some can be ignored due to implementation changes i.e...
 		tPosRestore = t.localPosition;
 		tRotRestore = t.localRotation;
-		crystalPosRestore = crystal.transform.localPosition;
-		crystalRotRestore = crystal.transform.localRotation;
-		laserRingPosRestore = laserRing.transform.localPosition;
-		laserRingRotRestore = laserRing.transform.localRotation;
-		laserRingSmallPosRestore = laserRingSmall.transform.localPosition;
-		laserRingSmallRotRestore = laserRingSmall.transform.localRotation;
+		crystalPosRestore = crystal.transform.localPosition; //ignore
+		crystalRotRestore = crystal.transform.localRotation; //ignore
+		laserRingPosRestore = laserRing.transform.localPosition; //ignore
+		laserRingRotRestore = laserRing.transform.localRotation; //ignore
+		laserRingSmallPosRestore = laserRingSmall.transform.localPosition; //ignore
+		laserRingSmallRotRestore = laserRingSmall.transform.localRotation; //ignore
 
 		cam = Player.INSTANCE.GetComponent<Camera> ();
 
@@ -68,8 +70,10 @@ public class MountedLaserMain : InteractableObject {
 
 	void FixedUpdate() {
 		if (transform.rotation == tRotRestore) {
+			//main body floats only when in its neutral 
 			t.Translate (0.0f, Mathf.Sin (Time.fixedTime * Mathf.PI * floatRate) * amplitude, 0.0f);
 		}
+		//float children parts of the laser (outer rings and inner crystal)
 		crystal.transform.Translate (0.0f, Mathf.Sin (Time.fixedTime * Mathf.PI * 1.5f *floatRate) * amplitude * 0.9f, 0.0f);
 		laserRing.transform.Translate (0.0f, Mathf.Sin (Time.fixedTime * Mathf.PI * floatRate * 0.25f) * amplitude * 0.2f, 0.0f);
 		laserRingSmall.transform.Translate (0.0f, Mathf.Sin (Time.fixedTime * Mathf.PI * floatRate * 0.5f) * amplitude * 0.2f, 0.0f);
@@ -81,15 +85,21 @@ public class MountedLaserMain : InteractableObject {
 		}
 	}
 
+
+	/// <summary>
+	/// Reaim this instance.
+	/// I am trying to create a class that changes the orientation of the gun each time the player interacts with it.
+	/// It would have its rotation lerped with each interaction locking further interaction till the movement was done.
+	/// </summary>
 	IEnumerator Reaim() {
 		interactable = false;
 		Quaternion newAimAngles = tRotRestore;
 		aim++;
 		float elapsedTime = 0.0f;
 
-		switch (aim) {
+		switch (aim) { //enumerate between all the possible directions toggle by player interactions
 		case Aim.NORTH:
-			{
+			 {
 				newAimAngles = currentAim;
 				newAimAngles.x += 90.0f;
 				break;
@@ -114,6 +124,7 @@ public class MountedLaserMain : InteractableObject {
 			}
 		}
 
+		//lerp the movement of the laser main body
 		while (elapsedTime < aimDuration){
 			elapsedTime += Time.deltaTime;
 			transform.rotation = Quaternion.Lerp(currentAim, newAimAngles, elapsedTime / aimDuration);
