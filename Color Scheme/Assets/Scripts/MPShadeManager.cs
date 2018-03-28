1da669d0b1266d3c9ec34d9145475fdc970a2eb2
Elaine Chao <chaoe@rpi.edu>
2018-03-27 23:00:44 -0400

    Color wheel room wall, edited shade scripts, bucket color ui

--- Color Scheme/Assets/Scripts/MPShadeManager.cs
+++ Color Scheme/Assets/Scripts/MPShadeManager.cs
@@ -2,43 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MPShadeManager : SimplePaintableObject
+public class MPShadeManager : MonoBehaviour
 {
-    Renderer r;
-    public Transform platform;
-    public float maxSpeed;
 
-    public Color shadeColor;
-    public Color[] changeArray;
-    public Color killColor;
+    public MPShade[] shades;
 
     void Start()
     {
-        enabled = true;
+
     }
 
     void Update()
     {
-        while (enabled)
-        {
-            StartCoroutine(changeColor(platform.gameObject));
-        }
+        int index = Random.Range(0, shades.Length);
+        shades[index].called = true;
     }
 
-    IEnumerator changeColor(GameObject obj)
-    {
-        enabled = false;
-        yield return new WaitForSecondsRealtime(6);
-        int index = Random.Range(0, changeArray.Length);
-        obj.GetComponent<Renderer>().material.color = changeArray[index];
-        enabled = true;
-    }
-
-    public override void Paint(Color c)
-    {
-        if (c == killColor)
-        {
-            Destroy(this.gameObject);
-        }
-    }
 }
