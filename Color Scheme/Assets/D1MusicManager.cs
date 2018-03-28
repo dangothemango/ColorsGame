f7d629e408e6add1512f079664bc53fb29564baf
myoder345 <myoder345@gmail.com>
2018-03-28 16:26:03 -0400

    Started integrating the music. Unrelated, bucket is no longer embedded in floor

---
+++ Color Scheme/Assets/D1MusicManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D1MusicManager : MonoBehaviour {
+    [SerializeField]
+    AudioSource[] musicians;
+
+	// Use this for initialization
+	void Start () {
+        musicians = GetComponents<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Color Scheme/Assets/DisableAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableAudio : MonoBehaviour {
+    AudioSource drone;
+
+	// Use this for initialization
+	void Start () {
+        drone = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == Player.INSTANCE.gameObject)
+        {
+            drone.loop = false;
+        }
+    }
+}
