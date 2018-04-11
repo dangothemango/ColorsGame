1d661a9e728769b9d979e74208fc478c5a631212
khabane <seretsi@users.noreply.github.com>
2018-04-11 13:24:20 -0400

    mid progress commit - menu contructions

--- Color Scheme/Assets/PauseMenuScript.cs
+++ Color Scheme/Assets/PauseMenuScript.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseMenuScript : MonoBehaviour {
+	
+
+	void Start(){
+
+	}
 
 	void OnEnable(){
-		Time.timeScale = 0.0f;
-
 	}
 
 	// Update is called once per frame
 	void Update () {
+	}
+
+	public void Resume() {
+		Player.INSTANCE.PauseGame ();
+	}
+
+	public void ExitToMainMenu (){
+
+	}
+
+	public void Exit(){
+
 	}
 }
