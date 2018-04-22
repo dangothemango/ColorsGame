c41395ae49bcf0e732626aa50b0ab507341e2e1e
khabane <seretsi@users.noreply.github.com>
2018-04-22 02:00:12 -0400

    shade code done
    Untested

--- Color Scheme/Assets/Scripts/EyedropperScript.cs
+++ Color Scheme/Assets/Scripts/EyedropperScript.cs
@@ -15,8 +15,8 @@ public class EyedropperScript : PlayerItem {
 	bool hasShade = false;
 
 	void Awake() {
-		//sampleAudio = GetComponent<AudioSource> ();
-		//releaseAudio = GetComponent<AudioSource> ();
+		// sampleAudio = GetComponent<AudioSource> ();
+		// releaseAudio = GetComponent<AudioSource> ();
 		if (!shade) {
 			//shade = GetComponentInChildren
 		}
@@ -34,12 +34,24 @@ public class EyedropperScript : PlayerItem {
 	}
 
 	public override bool CanUseOn(InteractableObject target) {
-		return target == shade ? true : false;
+		return target.GetComponent<Shade>() != null;
 	}
 
 	//do what eyedropper do!!
 	public override void UseOn (InteractableObject target) {			
-		
+		shade = target.GetComponent<Shade> ();
+        shade.shadeIsInteractedWith = true;
+
+        StartCoroutine(SampleShade(shade));
+		//through interactable script attached to Shade grab Shade script
+		//while interacting
+			//freeze shade
+			//lower alpha value of its colour
+			//emit particles from transform position 
+			//direct them to particle attractor at front of eyedropper tool
+			//if battery
+		//call its deposit function
+		//
 	}
 
 	public override void Filter (Color c){
@@ -52,8 +64,24 @@ public class EyedropperScript : PlayerItem {
 	}
 
 	public override Sprite GetTooltipIcon(InteractableObject io, out Color c) {
-
 		c = currentColor;
 		return primaryTooltip;
 	}
+
+    private IEnumerator SampleShade(Shade shade) {
+        Color c = shade.GetComponent<Renderer>().material.color;
+        while (c.a >= 0.0f) {
+            if (Input.GetKeyUp(GameManager.INSTANCE.INTERACT)) {
+                shade.shadeIsInteractedWith = false; 
+                yield return null;
+            }
+            c.a -= 0.1f;
+            Debug.Log(c.a);
+            yield return new WaitForSeconds(0.15f);
+        }
+        currentColor = shade.shadeColor;
+        currentColor.a = 1.0f;
+        Destroy(shade.gameObject, 0.0f);
+        yield return null;
+    }
 }
